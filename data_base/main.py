89515957640fa08dc93c670a244fc3037a91ca35
vcslav-v <vcslav.v@gmail.com>
2021-02-21 00:27:38 +0300

    fix lint

--- data_base/main.py
+++ data_base/main.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 SQLALCHEMY_DATABASE_URI = (
     'postgresql+psycopg2://postgres:{password}@{db}/postgres'.format(
         db=environ.get('DB'),
-        password=environ.get('DB_PASSWORD')
+        password=environ.get('DB_PASSWORD'),
     )
 )
 

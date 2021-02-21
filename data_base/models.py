b788d69736e50a9ab69e5125138d9862ec56c7b4
vcslav-v <vcslav.v@gmail.com>
2021-02-21 19:09:57 +0300

    add models

--- data_base/models.py
+++ data_base/models.py
@@ -1,30 +1,45 @@
 """DataBase models."""
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, ForeignKey, Integer, Text
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 
-class TelegramUsers(Base):
+class TelegramUser(Base):
     """Telegram users."""
 
     __tablename__ = 'telegram_users'
 
     id = Column(Integer, primary_key=True)
+
     telegram_id = Column(Integer)
+    stories = relationship('Story', back_populates='author')
 
 
-class Stories(Base):
+class Story(Base):
     """Srories."""
 
     __tablename__ = 'stories'
 
     id = Column(Integer, primary_key=True)
 
+    author_id = Column(Integer, ForeignKey('telegram_users.id'))
+    author = relationship('TelegramUser', back_populates='stories')
 
-class Messages(Base):
+    messages = relationship('Message', back_populates='story')
+
+
+class Message(Base):
     """Messages."""
 
     __tablename__ = 'messages'
 
     id = Column(Integer, primary_key=True)
+
+    story_id = Column(Integer, ForeignKey('stories.id'))
+    story = relationship('Story', back_populates='messages')
+
+    message = Column(Text)
+    parent_id = Column(Integer, ForeignKey('messages.id'))
+    link = relationship('Message', lazy='joined', join_depth=1)

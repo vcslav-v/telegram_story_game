37f182171934c18b960daddd8762f9c7565b2d44
vcslav-v <vcslav.v@gmail.com>
2021-02-21 19:10:20 +0300

    add get_user_or_make_if_new

--- data_base/main.py
+++ data_base/main.py
@@ -2,6 +2,9 @@
 from os import environ
 
 from sqlalchemy import create_engine
+from sqlalchemy.orm.session import sessionmaker
+
+from data_base import models
 
 SQLALCHEMY_DATABASE_URI = (
     'postgresql+psycopg2://postgres:{password}@{db}/postgres'.format(
@@ -11,3 +14,26 @@ SQLALCHEMY_DATABASE_URI = (
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
+session = sessionmaker(bind=engine)()
+
+
+def get_user_or_make_if_new(telegram_id: int) -> models.TelegramUser:
+    """Return user if one exist or add new user to db.
+
+    Parameters:
+        telegram_id: telegram id
+
+    Returns:
+        User object
+    """
+    user = session.query(models.TelegramUser).filter_by(
+        telegram_id=telegram_id,
+    ).first()
+    if user:
+        return user
+
+    user = models.TelegramUser(telegram_id=telegram_id)
+
+    session.add(user)
+    session.commit()
+    return user
